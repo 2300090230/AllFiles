32e257edb56c3225e34f4484b64680873ca804ba
Laxman <2300090230@kluniversity.in>
2025-10-29 16:10:34 +0530

    added manual docker

---
+++ DockerFullStack-AWS/config.js
@@ -0,0 +1,8 @@
+const config = {
+    //url: "http://localhost:2030/apionlineart"
+    //url: "http://localhost:3025"
+    //keep here the aws url
+    url: "http://ec2-3-19-246-14.us-east-2.compute.amazonaws.com:2001"
+}
+//backend host port no
+export default config;
